1774bd0a66f5087b26e93909c6e4441a02c1c5df
Sergey Zak <93543663+Nedravka@users.noreply.github.com>
2022-06-29 00:12:18 +0400

    added edits after code review

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,37 +1,29 @@
-import json
-import os
 from pathlib import Path
-from urllib.parse import unquote, urlsplit
 
 import requests
 
+from utils import save_images
 
 LINK_API_SPACEX = 'https://api.spacexdata.com/v3/launches/66'
 
 
-def create_path(path):
-    return Path(f'{path}').mkdir(parents=True, exist_ok=True)
-
-
-def file_extension(url):
-    name_of_file = urlsplit(url).path
-    file_extansion = os.path.splitext(unquote(name_of_file))
-    return file_extansion[-1]
-
-
 def fetch_spacex_last_launch(link_api, path):
-    create_path(path)
-    api_json = requests.get(link_api)
-    api_json.raise_for_status()
-    dict_api = json.loads(api_json.text)
+
+    Path(f'{path}').mkdir(parents=True, exist_ok=True)
+
+    response_api_spacex = requests.get(link_api)
+    response_api_spacex.raise_for_status()
+    dict_api = response_api_spacex.json()
 
     for image in dict_api['links']['flickr_images']:
         response = requests.get(image)
-        img_name = image.split('/')
-        with open(f'{path}\\{img_name[-1]}', 'wb') as picture:
-            picture.write(response.content)
+        *_, img_name = image.split('/')
+
+        # with open(f'{path}\\{img_name}', 'wb') as picture:
+        #     picture.write(response.content)
+        save_images(f'{path}\\{img_name}', response)
 
 
 if __name__ == '__main__':
 
-    fetch_spacex_last_launch(LINK_API_SPACEX, 'images/spasex')
+    fetch_spacex_last_launch(LINK_API_SPACEX, 'images/spacex')
