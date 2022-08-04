2742c5d3ff17fb3eeb515668fb184177a1e67408
Sergey Zak <93543663+Nedravka@users.noreply.github.com>
2022-08-04 15:21:25 +0400

    correct name of variable

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -4,26 +4,24 @@ import requests
 
 from utils import save_images
 
-LINK_API_SPACEX = 'https://api.spacexdata.com/v3/launches/66'
+API_SPACEX_LINK = 'https://api.spacexdata.com/v3/launches/66'
 
 
-def fetch_spacex_last_launch(link_api, path):
+def fetch_spacex_last_launch(api_link, path):
 
     Path(f'{path}').mkdir(parents=True, exist_ok=True)
 
-    response_api_spacex = requests.get(link_api)
-    response_api_spacex.raise_for_status()
-    dict_api = response_api_spacex.json()
+    api_spacex_response = requests.get(api_link)
+    api_spacex_response.raise_for_status()
+    api_spacex_response_json = api_spacex_response.json()
 
-    for image in dict_api['links']['flickr_images']:
-        response = requests.get(image)
+    for image in api_spacex_response_json['links']['flickr_images']:
+        last_launch_image = requests.get(image)
         *_, img_name = image.split('/')
 
-        # with open(f'{path}\\{img_name}', 'wb') as picture:
-        #     picture.write(response.content)
-        save_images(f'{path}\\{img_name}', response)
+        save_images(f'{path}\\{img_name}', 'wb', last_launch_image)
 
 
 if __name__ == '__main__':
 
-    fetch_spacex_last_launch(LINK_API_SPACEX, 'images/spacex')
+    fetch_spacex_last_launch(API_SPACEX_LINK, 'images/spacex')
